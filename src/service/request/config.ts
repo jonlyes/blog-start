b92fee4b96a953162cf9ac205327e3bd3dfa1eb4
jonlyes <3236526598@qq.com>
2023-11-01 15:13:14 +0800

    小修

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -2,8 +2,7 @@ let baseURL: string = "";
 
 switch (process.env.NODE_ENV) {
   case "development":
-    // baseURL = "http://localhost:8030";
-    baseURL = "http://8.134.109.167:8030";
+    baseURL = "http://localhost:8030";
 
     break;
   case "production":
